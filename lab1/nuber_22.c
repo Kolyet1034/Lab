629889db69b7b73db366bdb54b94d26d93f7c57f
Kolyet1034 <elizavetaKocheshkova1034@gmail.com>
2026-09-20 01:03:52 +0300

    first commit

---
+++ lab1/nuber_22.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main(void){
+    float z_1, z_2, z_3;
+
+    for (float x_n = 1.0; x_n <= 3.0; x_n += 1.3){
+        for (float a_n = 2.0; a_n <= 4.0; a_n += 1.3){
+            z_1 = pow((a_n - x_n) / x_n, 0.5);
+            z_2 = cos(a_n * a_n / x_n);
+            z_3 = cos((x_n * pow(a_n, 1/3))/(a_n + 2.5))/sin((x_n * pow(a_n, 1/3))/(a_n + 2.5));
+            if (z_1 > z_2) {
+                if (z_2 > z_3){
+                    printf("%.1f\n", z_1);
+                } else {
+                    if (z_1 > z_3){
+                        printf("%.1f\n", z_1);
+                    } else {
+                        printf("%.1f\n", z_3);
+                    }
+                }
+            } else{
+                if (z_2 > z_3){
+                    printf("%.1f\n", z_2);
+                } else {
+                    printf("%.1f\n", z_3);
+                }
+            }
+        }
+    }
+
+    return 0;
+}
